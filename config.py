f76c4d3fad3ab963dd43550fb60212db31b8d99d
administrador <alecurfon@gmail.com>
2020-02-28 14:16:25 +0000

    setting up the installer

--- config.py
+++ config.py
@@ -12,7 +12,8 @@ DATABASE_CONNECT_OPTIONS = {}
 SECRET_KEY = '\xfc\xfe\xad\xe1u\xd3=\xfd?\x00\xb6\xfeQ\x176\x17C\xd6\xfa\xf2\xfbK\x1f\xa0\xd6lx~\xcev.\xb4'
 
 if __name__ == '__main__':
-    from models import User
+    from app import app, db
+    from app.models import User
 
     db.create_all()
     print('''>> Creating the two user accounts.
