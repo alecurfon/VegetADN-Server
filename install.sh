b1204d723a8d143fc26419438313e05f047f9e4d
administrador <alecurfon@gmail.com>
2020-02-28 13:39:58 +0000

    setting up the installer

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ printf '\n>> Installing the needed tools ...\n'
 sudo apt -q -y install python3.7 python3-venv postgresql postgresql-contrib perl
 # >/dev/null 2>&1  &>/dev/null ? python-psycopg2
 printf '\n>> Setting up the Python virtual environment ...\n'
-python3.7 -q -m venv ../.vegetadn-virtualenv
-source ../.vegetadn-virtualenv/bin/activate
+python3.7 -q -m venv ./.vegetadn-virtualenv
+source ./.vegetadn-virtualenv/bin/activate
 pip install -q -r ./requirements.txt
 
 printf '\n>> Setting up the PostgreSQL service ...\n'
@@ -14,14 +14,14 @@ sudo service postgresql start
 sudo service postgresql restart
 sudo useradd vegetadn
 sudo -u postgres createuser -g postgres -w -D -A vegetadn
+sudo -u postgres createuser -g vegetadn -w -D -A "$(whoami)"
 sudo -u postgres createdb -O vegetadn vegetadn
 sudo /etc/init.d/postgresql reload
-psql vegetadn vegetadn < ./app/models/biosql_scheme.sql
-python ./app/models/user_account.py
+sudo -u vegetadn psql vegetadn < ./app/models/biosql_scheme.sql
 
 # loading ncbi taxonomy
 chmod +x ./app/models/load_ncbi_taxonomy.pl
-print '\n>> Would you like to load the taxonomy of the NCBI? This could take a while. [y/N]: '
+printf '\n>> Would you like to load the taxonomy of the NCBI? This could take a while. [y/N]: '
 read response
 if [ "$response" = "y" ]
 then
@@ -29,6 +29,7 @@ then
   perl -MCPAN -e 'install DBD::Pg'
   ./app/models/load_ncbi_taxonomy.pl --dbname vegetadn --driver Pg --dbuser vegetadn --download true
 fi
-psql vegetadn vegetadn < ./app/models/search_scheme.sql
+sudo -u vegetadn psql vegetadn < ./app/models/search_scheme.sql
+python ./app/__init__.py
 
 printf '\n>> DONE\n'
