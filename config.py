5568bf7d6fbf4742633e8a930b96aa40fe0e5697
administrador <alecurfon@gmail.com>
2020-02-29 14:01:26 +0000

    setting up the installer

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ _basedir = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = False
 TESTING = False
-SQLALCHEMY_DATABASE_URI = 'postgresql://vegetadn:vegetadn@localhost:5432/vegetadn'
+SQLALCHEMY_DATABASE_URI = 'postgresql://vegetadn:@localhost:5432/vegetadn'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 DATABASE_CONNECT_OPTIONS = {}
 SECRET_KEY = '\xfc\xfe\xad\xe1u\xd3=\xfd?\x00\xb6\xfeQ\x176\x17C\xd6\xfa\xf2\xfbK\x1f\xa0\xd6lx~\xcev.\xb4'
--- install.sh
+++ install.sh
@@ -1,33 +1,40 @@
 cd $( dirname $0 )
+sudo chmod +x ./app/models/load_ncbi_taxonomy.pl
 
 printf '\n>> Installing the needed tools ...\n'
 sudo apt -qq -y install python3.7 python3-venv postgresql postgresql-contrib perl
-printf '\n>> Setting up the Python virtual environment ...\n'
-python3.7 -q -m venv ./.venv
-source ./.venv/bin/activate
-pip install -q -r ./requirements.txt
+perl -MCPAN -e 'install DBI'
+perl -MCPAN -e 'install DBD::Pg'
 
 printf '\n>> Setting up the PostgreSQL service ...\n'
 sudo service postgresql start
 sudo service postgresql restart
-sudo useradd vegetadn -p vegetadn
-sudo -u postgres createuser --superuser "$(whoami)"
-createuser -g "$(whoami)" -w -D -A vegetadn
-createdb vegetadn
-sudo /etc/init.d/postgresql reload
+printf '\n>> Setting up the system user "vegetadn" ...\n'
+sudo useradd vegetadn
+sudo passwd vegetadn
+sudo -u postgres createuser --superuser vegetadn
+
+printf '\n>> Log in as "vegetadn" ...\n'
+su vegetadn
+# sudo /etc/init.d/postgresql reload
 
 printf '\n>> Setting up the PostgreSQL scheme ...\n'
+createdb vegetadn
 psql vegetadn < ./app/models/biosql_scheme.sql
-chmod +x ./app/models/load_ncbi_taxonomy.pl
+
 printf '\n>> Would you like to load the taxonomy of the NCBI? This could take a while. [y/N]: '
 read response
 if [ "$response" = "y" ]
 then
-  perl -MCPAN -e 'install DBI'
-  perl -MCPAN -e 'install DBD::Pg'
   ./app/models/load_ncbi_taxonomy.pl --dbname vegetadn --driver Pg --dbuser vegetadn --download true
 fi
+
 psql vegetadn < ./app/models/search_scheme.sql
+
+printf '\n>> Setting up the Python virtual environment ...\n'
+python3.7 -q -m venv ./.venv
+source ./.venv/bin/activate
+pip install -q -r ./requirements.txt
 python ./config.py
 
 printf '\n>> DONE\n'
