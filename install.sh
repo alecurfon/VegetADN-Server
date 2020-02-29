a266e7ec9f3df24f84127cc77614a25a2ce6fab8
administrador <alecurfon@gmail.com>
2020-02-29 14:13:20 +0000

    setting up the installer

--- install.sh
+++ install.sh
@@ -3,8 +3,6 @@ sudo chmod +x ./app/models/load_ncbi_taxonomy.pl
 
 printf '\n>> Installing the needed tools ...\n'
 sudo apt -qq -y install python3.7 python3-venv postgresql postgresql-contrib perl
-perl -MCPAN -e 'install DBI'
-perl -MCPAN -e 'install DBD::Pg'
 
 printf '\n>> Setting up the PostgreSQL service ...\n'
 sudo service postgresql start
@@ -15,26 +13,27 @@ sudo passwd vegetadn
 sudo -u postgres createuser --superuser vegetadn
 
 printf '\n>> Log in as "vegetadn" ...\n'
-su vegetadn
 # sudo /etc/init.d/postgresql reload
 
 printf '\n>> Setting up the PostgreSQL scheme ...\n'
-createdb vegetadn
-psql vegetadn < ./app/models/biosql_scheme.sql
+sudo -u vegetadn createdb vegetadn
+sudo -u vegetadn psql vegetadn < ./app/models/biosql_scheme.sql
 
 printf '\n>> Would you like to load the taxonomy of the NCBI? This could take a while. [y/N]: '
 read response
 if [ "$response" = "y" ]
 then
+  perl -MCPAN -e 'install DBI'
+  perl -MCPAN -e 'install DBD::Pg'
   ./app/models/load_ncbi_taxonomy.pl --dbname vegetadn --driver Pg --dbuser vegetadn --download true
 fi
 
-psql vegetadn < ./app/models/search_scheme.sql
+sudo -u vegetadn psql vegetadn < ./app/models/search_scheme.sql
 
 printf '\n>> Setting up the Python virtual environment ...\n'
-python3.7 -q -m venv ./.venv
-source ./.venv/bin/activate
-pip install -q -r ./requirements.txt
-python ./config.py
+sudo -u vegetadn python3.7 -q -m venv ./.venv
+sudo -u vegetadn source ./.venv/bin/activate
+sudo -u vegetadn pip install -q -r ./requirements.txt
+sudo -u vegetadn python ./config.py
 
 printf '\n>> DONE\n'
