262dcdb0254bf7dd6872d1c6982145134e17d96b
administrador <alecurfon@gmail.com>
2020-03-02 12:01:32 +0000

    setting up the installer

--- execute.sh
+++ execute.sh
@@ -4,4 +4,4 @@ cd $( dirname $0 )
 source ./.venv/bin/activate
 export FLASK_APP=./run.py
 export FLASK_ENV=development
-flask run >/dev/null 2>&1 &
+flask run &
--- install.sh
+++ install.sh
@@ -1,45 +1,43 @@
+#!/bin/bash
+
 cd $( dirname $0 )
 
-printf '\n>> Insert the password for the system.\n'
-while true
-do
-  printf '\nPassword: ';read -s password;
-  printf '\nRepeat the same password: ';read -s match_pass;
-  [ "$password" == "$match_pass" ] && break
-  printf '\nThe password does not match. Try again.'
-done
-echo "$password">./password
-chmod 600 ./password
-
-printf '\n\n>> Installing the needed tools ...\n'
+printf '\n>> Installing the needed tools ...\n'
 sudo apt -qq -y install python3.7 python3-venv postgresql-11 postgresql-contrib perl
 
 printf '\n>> Setting up the PostgreSQL service ...\n'
 sudo service postgresql start
 sudo service postgresql restart
 
-printf '\n>> Setting up the system user "vegetadn" ...\n'
+printf '\n>> Creating the user "vegetadn" ...\nInsert the password for the database.\n'
+while true
+do
+  printf 'Password: ';read -s password;
+  printf 'Repeat the same password: ';read -s match_pass;
+  [ "$password" == "$match_pass" ] && break
+  printf '\nThe password does not match. Try again.\n';
+done
+echo "$password">./password;chmod 600 ./password;
 sudo useradd vegetadn
 yes "$password" | sudo passwd vegetadn
 sudo -u postgres createuser --superuser vegetadn
 echo "ALTER USER vegetadn WITH PASSWORD '$password' ;" | sudo -u postgres psql
 # sudo /etc/init.d/postgresql reload
 
-printf '\n>> Setting up the PostgreSQL scheme ...\n'
+printf '\n>> Loading the database schema ...\n'
 sudo -u vegetadn createdb vegetadn
 sudo -u vegetadn psql vegetadn < ./app/models/biosql_scheme.sql
+sudo -u vegetadn psql vegetadn < ./app/models/search_scheme.sql
 
 printf '\n>> Would you like to load the taxonomy of the NCBI? This could take a while. [y/N]: '
 read response
 if [ "$response" = "y" ]
 then
-  perl -MCPAN -e 'install DBI'
-  perl -MCPAN -e 'install DBD::Pg'
+  yes '' | perl -MCPAN -e 'install DBI'
+  yes '' | perl -MCPAN -e 'install DBD::Pg'
   sudo chmod +x ./app/models/load_ncbi_taxonomy.pl
-  ./app/models/load_ncbi_taxonomy.pl --dbname vegetadn --driver Pg --dbuser vegetadn --download true
+  yes '' | ./app/models/load_ncbi_taxonomy.pl --dbname vegetadn --driver Pg --dbuser vegetadn --download true
 fi
-
-sudo -u vegetadn psql vegetadn < ./app/models/search_scheme.sql
 
 printf '\n>> Setting up the Python virtual environment ...\n'
 python3.7 -q -m venv ./.venv
