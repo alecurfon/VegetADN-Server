095bdfb44601ad9b2c305003736d5fddb732a015
administrador <alecurfon@gmail.com>
2020-03-30 13:12:10 +0100

    resolving some mistakes

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 DBTYPE = 'postgresql'
 DBDRIVER = 'psycopg2'
-DBHOST = 'localhost:5432'
+DBHOST = 'localhost'
+DBPORT = '5432'
 DBNAME = 'vegetadn'
 DBUSER = 'vegetadn'
 with open(f'{basedir}/password', 'r') as file:
@@ -11,7 +12,7 @@ with open(f'{basedir}/password', 'r') as file:
 
 DEBUG = False
 TESTING = False
-SQLALCHEMY_DATABASE_URI = f'{DBTYPE}://{DBUSER}:{DBPASSWD}@{DBHOST}/{DBNAME}'
+SQLALCHEMY_DATABASE_URI = f'{DBTYPE}://{DBUSER}:{DBPASSWD}@{DBHOST}:{DBPORT}/{DBNAME}'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 DATABASE_CONNECT_OPTIONS = {}
 SECRET_KEY = '\xfc\xfe\xad\xe1u\xd3=\xfd?\x00\xb6\xfeQ\x176\x17C\xd6\xfa\xf2\xfbK\x1f\xa0\xd6lx~\xcev.\xb4'
